b7f65a8c048b762ea9c150002121a559ff5c0e26
ali-shariatii <a.shariatii91@gmail.com>
2020-05-17 13:52:10 -0400

    initial backup before adding php

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ modalCloser1.onclick = function() {
     modalFullWindow1.style.display = "none";
 }
 
+
+
 //modal2 func
 modalOpener2.onclick = function() {
     modalFullWindow2.style.display = "block";
