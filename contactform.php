02a6904a2822a5a021ce1b3c33a0e176b25c10e6
ali.shariati <a.shariatii91@gmail.com>
2020-06-12 04:05:51 -0400

    fixed nav bug & finalized form

---
+++ contactform.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="4;URL='https://alishariatii.com/'" />
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/work.css">
+    <link rel="stylesheet" href="css/testimonials.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <title>Portfolio | Ali Shariati</title>
+    <link rel="shortcut icon" type="image/png" href="img/favicon/favicon-256×256.ico">
+</head>
+<body>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {        
+    $name = test_input($_POST['name']);
+    $phone = test_input($_POST['phone']);
+    $email = test_input($_POST['email']);
+    $name = test_input($_POST['subject']);
+    $name = test_input($_POST['message']);
+    
+    $mailTo = "info@alishariatii.com";
+    $headers = "From: ".$email;
+    $body = $name." has sent a message via your portfolio online form."."\n\n"."Sender's contact info:".".\n"."Email: ".$email.".\n"."Phone: ".$phone.".\n"."Message: ".$message;
+
+    mail($mailTo, $subject, $body, $headers);
+}
+
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);  
+    return $data;     
+}
+?>
+
+<header id="home-grid">
+    <div id="header-grid-shade">    
+        <div id="message-grid-item">
+            <p>Your message has been received. I'll be in touch soon. ;)</p>
+            <p>Click<a href="https://alishariatii.com/"> here</a> if you did not redirect to the main page.</p>
+            <img src="img/logo.svg" id="logo4" alt="Redirecting in 4 Seconds ...">
+        </div>
+    </div>
+</header>
+
+<!--JS LINKS-->
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
